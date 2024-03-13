82721193c53df126a5b494ba1311df1f6f8fe56b
Novo1999 <neptune.navo@gmail.com>
2024-03-13 15:57:50 +0600

    step 4

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
-use std::env;
-use std::path::PathBuf;
-
 use anyhow::{Context, Result};
-use clap::Parser;
+use serde::{Deserialize, Serialize};
+use std::path::PathBuf;
+use std::{env, string};
 
-fn main() -> Result<()> {
+#[derive(Debug, Deserialize, Serialize)]
+struct Monitor {
+    name: String,
+    #[serde(default)]
+    script: Option<String>,
+    #[serde(default)]
+    result: Option<String>,
+    code: String,
+    #[serde(default)]
+    monitor_id: Option<u32>,
+    #[serde(default)]
+    #[serde(rename = "type")]
+    monitor_type: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct Monitors {
+    monitors: Vec<Monitor>,
+}
+
+// struct Result {
+//     value: i32,
+//     processed_at: i64,
+// }
+
+fn main() {
     // Get the command-line arguments
     let args: Vec<String> = env::args().collect();
 
@@ -13,15 +37,17 @@ fn main() -> Result<()> {
     // Check if the argument count is correct
     if args.len() != 4 {
         println!("Usage: process_monitor -monitorFile /path/to/given/monitors.json/file");
+        return;
     }
 
-    // Check if the first argument is "-monitorFile"
     if args[1] != "process_monitor" {
         println!("Usage: process_monitor -monitorFile /path/to/given/monitors.json/file");
+        return;
     }
 
     if args[2] != "-monitorFile" {
         println!("Usage: process_monitor -monitorFile /path/to/given/monitors.json/file");
+        return;
     }
 
     // Get the file path from the second argument
@@ -30,9 +56,8 @@ fn main() -> Result<()> {
     let content: String = std::fs::read_to_string(file_path)
         .with_context(|| format!("Could not read file `{}`", file_path))?; // this shows a nice error by using the anyhow library
 
-    // Use the file path as needed
-    println!("File path: {}", file_path);
-    println!("{}", content);
+    let monitors: Monitors = serde_json::from_str(&content)
+        .with_context(|| format!("Failed to parse JSON data from file `{}`", file_path))?;
 
-    Ok(())
+    println!("{:?}", monitors);
 }
